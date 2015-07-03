15fc8bcdf9df5c3572fe4d0e789fe0ed29c2a5d8
Cees de Groot <cees@pagerduty.com>
2015-07-03 12:14:37 -0400

    Some more design ideas

--- src/main/scala/com/evrl/unclever/package.scala
+++ src/main/scala/com/evrl/unclever/package.scala
@@ -1,22 +1,71 @@
 package com.evrl
 
-import java.sql.{Connection, ResultSet}
+import java.sql.{Connection, SQLException}
 
-trait Query {
-  def in(conn: Connection): ConnectedQuery
-}
-
-trait ConnectedQuery {
-  def map[T](m: unclever.RowMapper[T]): List[T]
-}
-
-class QueryImpl(sql: String) extends Query {
-  override def in(conn: Connection): ConnectedQuery = ???
-}
+import scala.language.implicitConversions
 
 package object unclever {
 
-  type RowMapper[T] = ResultSet => T
+  /**
+   * A Query object. Typically created from a SQL string.
+   */
+  trait Query {
+    def withParams(args: Any*): Query
+    def in(conn: Connection): ConnectedQuery
+    def executeIn(conn: Connection): Unit
+  }
 
-  implicit def stringToQuery(s: String): Query = new QueryImpl(s)
+  /**
+   * A wrapper around java.sql.ResultSet that makes it nicer for Scala
+   * people.
+   */
+  trait ResultSetRow {
+    def col(i: Int): DbValue
+    def col(s: String): DbValue
+  }
+
+  /**
+   * A value of a single column in a single row. Can be any type
+   * supported by ResultSet.
+   */
+  trait DbValue
+
+  /**
+   * A query that has been assigned to a connection.
+   */
+  trait ConnectedQuery {
+    def onError(handler: (SQLException) => Unit): ConnectedQuery = ???
+
+    /**
+     * Execute the query and map the result set using the
+     * rowmapper.
+     * @param m A function to extract a row from a java.sql.ResultSet
+     * @tparam T The type of the result
+     * @return A (possibly empty) list of results
+     */
+    def map[T](m: unclever.RowMapper[T]): Seq[T]
+
+
+  }
+
+  /**
+   * Type that describes how to map from a ResultSet to a T
+   *
+   * @tparam T
+   */
+  type RowMapper[T] = ResultSetRow => T
+
+  // Bunch of default mappers
+  implicit def dbValueToInt(v: DbValue): Int = ???
+  implicit def dbValueToString(v: DbValue): String = ???
+
+  // Scala doesn't do this for you, alas.
+  implicit def optDbValueToInt(ov: Option[DbValue]): Option[Int] = ???
+
+  /**
+   * Implicitly convert a string to a Query object.
+   * @param s
+   * @return
+   */
+  implicit def asQuery(s: String): Query = ???
 }
--- src/test/scala/com/evrl/unclever/BasicUsage.scala
+++ src/test/scala/com/evrl/unclever/BasicUsage.scala
@@ -1,18 +1,56 @@
 package com.evrl.unclever
 
-import java.sql.{Connection, ResultSet}
+import java.sql.Connection
 
 import org.scalamock.scalatest.MockFactory
-import org.scalatest.{ShouldMatchers, FlatSpec}
+import org.scalatest.{FlatSpec, ShouldMatchers}
 
 class BasicUsage extends FlatSpec with ShouldMatchers with MockFactory {
 
+  val connection = mock[Connection]
+
   "unclever" should "make querying nice and simple" in {
+    val query = "select id from emp where emp = ?"
 
-    val query = "select id from emp where emp = 1"
-    val mapper: RowMapper[Int] = r => r.getInt(1)
-    val connection = mock[Connection]
+    val result: Option[Int] = query.withParams("1").in(connection).map(_.col(1)).headOption
+  }
 
-    val result: Option[Int] = query.in(connection).map(mapper).headOption
+  it should "be more scala-like than JDBC Template" in {
+    // Copying from
+    // http://docs.spring.io/spring/docs/current/spring-framework-reference/html/jdbc.html#jdbc-JdbcTemplate-examples-query
+    //
+    //    List<Actor> actors = this.jdbcTemplate.query(
+    //      "select first_name, last_name from t_actor",
+    //      new RowMapper<Actor>() {
+    //        public Actor mapRow(ResultSet rs, int rowNum) throws SQLException {
+    //          Actor actor = new Actor();
+    //          actor.setFirstName(rs.getString("first_name"));
+    //          actor.setLastName(rs.getString("last_name"));
+    //          return actor;
+    //        }
+    //      });
+    //
+
+    case class Actor(first: String, last: String)
+
+    // I think this is more concise ;-). By the way, in a past life I have done
+    // performance testing, and saw - at least with the MySQL JDBC Driver - a significant
+    // difference between fetch-by-name and fetch-by-column-index. I prefer the fast version.
+
+    val results: Seq[Actor] = "select first_name, last_name"
+      .in(connection).map(r => Actor(r.col(1), r.col(2)))
+  }
+
+  it should "be able to execute arbitrary statements" in {
+    "create table emp(id int, mgr_id int, name)".executeIn(connection)
+  }
+
+  it should "support error handling" in {
+    val result: Option[Int] = "select * from nonexistent_table"
+      .in(connection).onError(e => throw new Exception(e)).map(_.col(1)).headOption
+
+    // Or, using the default handling
+    val nada: Option[_] = "select * from nonexistent_table".in(connection).map(_.col(1)).headOption
+    nada should be(None)
   }
 }
